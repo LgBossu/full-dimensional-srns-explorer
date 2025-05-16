95c721f1ec25b3e8ef8503939eafebc649f6e722
LgBossu <luigiduluco@gmail.com>
2025-05-16 16:04:48 +0200

    Deleted UniformNormalizedSampler class due to uselessness

--- code/samplers.py
+++ code/samplers.py
@@ -25,38 +25,6 @@ class Sampler(ABC):
         pass
 
 
-class UniformNormalizedSampler:
-    """
-    A sampler that generates uniformly distributed samples in the unit simplex (general behaviors).
-    """
-
-    # Class is UNUSED and is considered DEPRECATED for the time being.
-
-    def __init__(self, delta: int, m: int, z: bool):
-        """
-        Initialize the sampler.
-        """
-        if delta != 2 or m != 2 or not z:
-            logger.warning("Expect unexpected behaviors for delta =! 2 or m =! 2 or z = False")
-        self.delta = delta
-        self.m = m
-        self.z = z
-        self.np_sampling_shape = (self.z + 1, self.delta**2, self.m**2)
-
-    def sample(self) -> RoutedBehavior:
-        sampled = np.random.dirichlet(np.ones(self.delta**2), (self.z + 1) * self.m**2)
-
-        if self.z:
-            sample_s = sampled[: self.m**2, :].T
-            sample_l = sampled[self.m**2 :, :].T
-            sample = np.array([sample_s, sample_l])
-            res = RoutedBehavior(sample)
-        else:
-            raise NotImplementedError("Sampling for z = False is not implemented.")
-
-        return res
-
-
 class NoSignalingSampler:
     def __init__(self, delta: int, m: int, z: bool = True):
         """
