d5cd9ed8a90847c6228a07d8c892c44b89482c05
LgBossu <luigiduluco@gmail.com>
2025-05-27 15:04:17 +0200

    Introduced a QuotientHyperplanes class to test permutations on hyperplanes and get only equivalence classes

--- code/extract_hyperplanes.py
+++ code/extract_hyperplanes.py
@@ -1,3 +1,5 @@
+from itertools import permutations
+
 import behaviors
 import no_signaling_sets
 import numpy as np
@@ -113,7 +115,7 @@ class HyperplanesExtractor:
         self.m = m
         self.non_srns_samples = non_srns_samples
 
-    # HYPERPLANE UTILS
+    # UTILS
     def scale_down_vector(
         vector: np.ndarray,
         atol: float = 1e-10,
@@ -166,25 +168,6 @@ class HyperplanesExtractor:
         else:
             return vector / np.linalg.norm(vector)
 
-    def canonicalize(eq: np.ndarray) -> np.ndarray:
-        """
-        Canonicalizes a hypeperplane equation by flattening its array if needed,
-        and setting the first nonzero element to be positive.
-
-        This enables consistent representation of hyperplanes, for later
-        comparison in sets.
-
-        Note that :
-        - This may switch the sign of the hyperplane equation (ie, the normal vector's direction).
-        - This does not guarantee that the hyperplane is normalized (ie, unit length).
-        - This returns a 1D numpy array.
-        """
-        flat = eq.flatten()
-        idx = np.flatnonzero(flat)
-        if idx.size and flat[idx[0]] < 0:
-            flat = -flat
-        return flat
-
     # HYPERPLANE EXTRACTION
     def extract_from_vector(
         self,
@@ -233,3 +216,187 @@ class HyperplanesExtractor:
                 hyperplanes.append(None)  # Append None for index consistency
 
         return hyperplanes
+
+
+class QuotientHyperplanes:
+    def __init__(self, delta: int, m: int, hyperplanes: list[np.ndarray]) -> None:
+        """
+        Initializes the QuotientHyperplanes with the given parameters.
+
+        :param delta: The delta value for the no-signaling set.
+        :param m: The m value for the no-signaling set.
+        :param hyperplanes: A list of hyperplane equations (numpy arrays).
+        """
+        self.delta = delta
+        self.m = m
+        self.hyperplanes = hyperplanes
+
+    # SIGN CONSISTENCY
+    def canonicalize(eq: np.ndarray) -> np.ndarray:
+        """
+        Canonicalizes a hypeperplane equation by flattening its array if needed,
+        and setting the first nonzero element to be positive.
+
+        This enables consistent representation of hyperplanes, for later
+        comparison in sets.
+
+        Note that :
+        - This may switch the sign of the hyperplane equation (ie, the normal vector's direction).
+        - This does not guarantee that the hyperplane is normalized (ie, unit length).
+        - This returns a 1D numpy array.
+        """
+        flat = eq.flatten()
+        idx = np.flatnonzero(flat)
+        if idx.size and flat[idx[0]] < 0:
+            flat = -flat
+        return flat
+
+    # FORMATTING FOR PERMUTATIONS
+    def format_hyperplane(
+        self,
+        equation: np.ndarray,
+    ) -> tuple[np.ndarray]:
+        # # By convention, we give equations a sign such that
+        # the first non-zero element is positive.
+        # first_nonzero_idx = np.flatnonzero(equation)[0]
+        # if equation[first_nonzero_idx] < 0:
+        #     equation = -equation
+        # TODO : ensure the function is applied to canonicalized hyperplanes.
+
+        el_S, el_L, el_NS = (
+            equation[0 : self.delta**2 * self.m**2],
+            equation[self.delta**2 * self.m**2 : 2 * self.delta**2 * self.m**2],
+            equation[2 * self.delta**2 * self.m**2 :],
+        )  # noqa: E501
+        el_S = el_S.reshape((self.delta, self.delta, self.m, self.m))
+        el_L = el_L.reshape((self.delta, self.delta, self.m, self.m))
+        el_NS = el_NS.reshape((self.delta,) * self.m)
+        return (el_S, el_L, el_NS)
+
+    def format_list_of_hyperplanes(
+        self,
+        equations: list[list[np.ndarray]],
+    ) -> list[tuple[np.ndarray]]:
+        formatted: list[tuple[np.ndarray]] = []
+        for el in equations:
+            formatted.append(self.format_hyperplane(el))
+
+        return formatted
+
+    def flatten_hyperplane(
+        self,
+        hyperplane: tuple[np.ndarray, np.ndarray, np.ndarray],
+    ) -> np.ndarray:
+        el_S, el_L, el_NS = hyperplane
+
+        equation = np.concatenate((el_S.flatten(), el_L.flatten(), el_NS.flatten()))
+
+        # # By convention, we give equations a sign such that the first non-zero element is positive.
+        # first_nonzero_idx = np.flatnonzero(equation)[0]
+        # if equation[first_nonzero_idx] < 0:
+        #     equation = -equation
+
+        return equation
+
+    def flatten_list_of_hyperplanes(
+        self,
+        hyperplanes: list[tuple[np.ndarray, np.ndarray, np.ndarray]],
+    ) -> np.ndarray:
+        flattened = []
+        for hyperplane in hyperplanes:
+            flattened.append(self.flatten_hyperplane(hyperplane))
+        return np.array(flattened, dtype=int)
+
+    # PERMUTATIONS
+    def permute_a(
+        self,
+        formatted_hyperplane: tuple[np.ndarray],
+    ) -> list[tuple[np.ndarray]]:
+        """Apply to a properly formatted hyperplane the permutation of coordinates
+        corresponding to relabelings of a values."""
+        permuted_hyperplanes = []
+        for perm in permutations(range(self.delta)):
+            permuted_S = formatted_hyperplane[0][list(perm), :, :, :]
+            permuted_L = formatted_hyperplane[1][list(perm), :, :, :]
+            permuted_NS = formatted_hyperplane[2]
+
+            permuted_hyperplanes.append((permuted_S, permuted_L, permuted_NS))
+        return permuted_hyperplanes
+
+    def permute_b(
+        self,
+        formatted_hyperplane: tuple[np.ndarray],
+    ) -> list[tuple[np.ndarray]]:
+        """Apply to a properly formatted hyperplane the permutation of coordinates
+        corresponding to relabelings of b values."""
+        permuted_hyperplanes = []
+        for perm in permutations(range(self.delta)):
+            permuted_S = formatted_hyperplane[0][:, list(perm), :, :]
+            permuted_L = formatted_hyperplane[1][:, list(perm), :, :]
+            permuted_NS = formatted_hyperplane[2][np.ix_(*([list(perm)] * self.m))]
+
+            permuted_hyperplanes.append((permuted_S, permuted_L, permuted_NS))
+        return permuted_hyperplanes
+
+    def permute_x(
+        self,
+        formatted_hyperplane: tuple[np.ndarray],
+    ) -> list[tuple[np.ndarray]]:
+        """Apply to a properly formatted hyperplane the permutation of coordinates
+        corresponding to relabelings of x values."""
+        permuted_hyperplanes = []
+        for perm in permutations(range(self.m)):
+            permuted_S = formatted_hyperplane[0][:, :, list(perm), :]
+            permuted_L = formatted_hyperplane[1][:, :, list(perm), :]
+            permuted_NS = formatted_hyperplane[2]
+
+            permuted_hyperplanes.append((permuted_S, permuted_L, permuted_NS))
+        return permuted_hyperplanes
+
+    def permute_y(
+        self,
+        formatted_hyperplane: tuple[np.ndarray],
+    ) -> list[tuple[np.ndarray]]:
+        """Apply to a properly formatted hyperplane the permutation of coordinates
+        corresponding to relabelings of y values."""
+        permuted_hyperplanes = []
+        for perm in permutations(range(self.m)):
+            permuted_S = formatted_hyperplane[0][:, :, :, list(perm)]
+            permuted_L = formatted_hyperplane[1][:, :, :, list(perm)]
+            permuted_NS = formatted_hyperplane[2]
+
+            permuted_hyperplanes.append((permuted_S, permuted_L, permuted_NS))
+        return permuted_hyperplanes
+
+    list_of_permutations = [permute_a, permute_b, permute_x, permute_y]
+
+    def permute(
+        self,
+        axis: int | str,
+        hyperplane: np.ndarray,
+    ) -> list[np.ndarray]:
+        """Permute a hyperplane along the specified axis."""
+        formatted_hyperplane = self.format_hyperplane(hyperplane)
+        # TODO : where do i canonicalize the hyperplane?
+
+        if isinstance(axis, str):
+            axis = axis.lower()
+            if axis == "a":
+                unformatted_res = self.permute_a(formatted_hyperplane)
+            elif axis == "b":
+                unformatted_res = self.permute_b(formatted_hyperplane)
+            elif axis == "x":
+                unformatted_res = self.permute_x(formatted_hyperplane)
+            elif axis == "y":
+                unformatted_res = self.permute_y(formatted_hyperplane)
+            else:
+                raise ValueError(f"Unknown axis: {axis}")
+        elif isinstance(axis, int):
+            if 0 <= axis < 4:
+                unformatted_res = self.list_of_permutations[axis](formatted_hyperplane)
+            else:
+                raise ValueError(f"Axis index out of range: {axis}")
+        else:
+            raise TypeError(f"Axis must be an int or a str, got {type(axis)}")
+
+        return self.flatten_list_of_hyperplanes(unformatted_res)
