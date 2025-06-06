af6fcab70e0401b4ee3092bcfad1588deba6f118
LgBossu <luigiduluco@gmail.com>
2025-06-06 11:19:25 +0200

    Implement main execution debugging equations generated in no-signaling set

--- code/solve_full_polytope.py
+++ code/solve_full_polytope.py
@@ -6,8 +6,27 @@ projects the vertices onto the experiment space, and then
 computes the hyperplane representation of the resulting polytope.
 """
 
-import numpy as np
+import behaviors
 import cdd
+import no_signaling_sets
+import numpy as np
 
 if __name__ == "__main__":
-    pass
+    delta = 2
+    m = 2
+
+    latent_set = no_signaling_sets.LatentSRNSSet(delta, m)
+    A, b = latent_set.get_equations()
+
+    max_mix_vector = behaviors.completely_mixed_behavior.get_vector()
+    with np.printoptions(threshold=np.inf, linewidth=np.inf):  # type: ignore
+        print("A:")
+        print(A)
+        print("b:")
+        print(b)
+        print("Maximally mixed vector:")
+        print(max_mix_vector)
+        print("A @ max_mix_vector:")
+        print(A @ max_mix_vector)
+        print("Equality check:")
+        print(A @ max_mix_vector == b)
